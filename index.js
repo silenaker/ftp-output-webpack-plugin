4670ee6af40f20362793efd99910dd996c5a3cf5
Jevin Shi <jevin.shj@gmail.com>
2019-03-17 23:42:15 +0800

    end ssh connection when compile done

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ function SftpOutputPlugin(options) {
   this.options = options
 }
 
-SftpOutputPlugin.prototype.apply = function (compiler) {
+SftpOutputPlugin.prototype.apply = function(compiler) {
   compiler.plugin('environment', () => {
     compiler.outputFileSystem = new SftpOutputFileSystem(this.options)
+  })
+  compiler.plugin('done', () => {
+    compiler.outputFileSystem.client.end()
   })
 }
 
