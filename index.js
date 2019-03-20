70be0f89c6e5aecbe7119841accf1345f11e490b
shij <shijixttb@163.com>
2019-03-21 00:29:04 +0800

    add options parser

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const SftpOutputFileSystem = require('./SftpOutputFileSystem')
+const url = require('url')
 
 function SftpOutputPlugin(options) {
+  if (!options) {
+    const i = process.argv.indexOf('--ftp')
+    if (!~i)
+      return console.warn('you must set --ftp option when you use ftp-output-webpack-plugin!')
+    options = process.argv[i + 1]
+    options = url.parse(options)
+    const auth = options.auth.split(':')
+    options.username = auth[0]
+    options.password = auth[1]
+    options = {
+      host: options.hostname,
+      port: options.port,
+      username: options.username,
+      password: options.password,
+      keepaliveInterval: 5000
+    }
+  }
   this.options = options
 }
 
 SftpOutputPlugin.prototype.apply = function(compiler) {
+  if (!this.options) return
   compiler.plugin('environment', () => {
     compiler.outputFileSystem = new SftpOutputFileSystem(this.options)
   })
